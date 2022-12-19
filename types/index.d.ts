7014cfdbe9ed68c37a41e74bdc9b6ba059f939ee
safazi <safazi@airmail.cc>
2022-12-19 15:06:26 -0600

    slash references

---
+++ types/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference no-default-lib="true"/>
+/// <reference types="@rbxts/compiler-types" />
+/// <reference path="drawing.d.ts" />
+/// <reference path="font.d.ts" />
+/// <reference path="point.d.ts" />
+/// <reference path="timer.d.ts" />
