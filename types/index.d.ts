443d3d0df289d71d42de6b18e4ea5d65e746cd40
safazi <safazi@airmail.cc>
2022-12-20 21:56:23 -0600

    types reference

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,6 @@
 /// <reference no-default-lib="true"/>
 /// <reference types="@rbxts/compiler-types" />
+/// <reference types="@rbxts/types" />
 /// <reference path="point/index.d.ts" />
 /// <reference path="drawing.d.ts" />
 /// <reference path="font.d.ts" />
