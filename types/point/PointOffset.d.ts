42bd82b545d0b4cae7b7ff1beeed0ce46c7d457a
safazi <safazi@airmail.cc>
2022-12-20 22:19:35 -0600

    constructor

--- types/point/PointOffset.d.ts
+++ types/point/PointOffset.d.ts
@@ -7,6 +7,7 @@ interface PointOffsetConstructor {
 	new (): PointOffset;
 	new (point: Point): PointOffset;
 	new (point: Point, offset: Vector2): PointOffset;
+	new (point: Point, x_offset: number, y_offset: number): PointOffset;
 }
 
 declare const PointOffset: PointOffsetConstructor;
