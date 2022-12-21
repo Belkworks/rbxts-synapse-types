5b3da0da1002039d6f06414c5fb825142976aa19
safazi <safazi@airmail.cc>
2022-12-20 21:55:06 -0600

    fix font

--- types/font.d.ts
+++ types/font.d.ts
@@ -1,11 +1,10 @@
 interface DrawFont {
-	GetTextBounds(font: this, text: string): Vector2;
+	GetTextBounds(size: number, text: string): Vector2;
 }
 
-// @hidden
 interface DrawFontConstructor {
 	Register: (font_data: string, options: object) => void;
-	RegisterDefault: (font_family_name: string, options: object) => Font;
+	RegisterDefault: (font_family_name: string, options: object) => DrawFont;
 	ListDefault: () => string[];
 }
 
